329d848f34f125158f0e156ca5afabd36cd917f7
Real96 <chicco.gaeta@gmail.com>
2019-11-12 18:00:52 +0100

    Little fix in Save Parameter algorithm

--- 3DSRNGTool/Controls/Frame_Misc.cs
+++ 3DSRNGTool/Controls/Frame_Misc.cs
@@ -64,7 +64,7 @@ namespace Pk3DSRNGTool
                 return false;
             if (BaseTime.Visible && (CurrentSeed != null && BaseTime.Text != ""))
             {
-                string sum = (f.Rand32 + ulong.Parse(BaseTime.Text, NumberStyles.HexNumber)).ToString();
+                string sum = ((f.Rand32 + ulong.Parse(BaseTime.Text, NumberStyles.HexNumber)) & 0xFFFFFFFF).ToString();
                 string currentSeedDec = ulong.Parse(CurrentSeed, NumberStyles.HexNumber).ToString();
                 if (sum.Length >= 2 && currentSeedDec.Length >= 2
                     && sum.Substring(sum.Length - 2) == currentSeedDec.Substring(currentSeedDec.Length - 2))
